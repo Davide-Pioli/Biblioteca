08fe389d0cea4d8b936108155089112961cda7b8
Davide <Davide.pioli.98@gmail.com>
2018-05-23 18:36:52 +0200

    Aggiunto CSS per il login
    Aggiunto il CSS per il login
    login.html è stato cambiato in login.php

--- addBooks.php
+++ addBooks.php
@@ -2,23 +2,28 @@
 <html>
 <head>
 	<title>Davide Library</title>
+	<link rel="stylesheet" type="text/css" href="styles/addBooks.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Yeseva One">
 </head>
 <body>
 	<?php
 		session_start();
 		if(isset($_SESSION['loginState'])){
 			echo "
-				<h1>Inserisci libri nel database</h1>
-				<input id='search' type='text' placeholder='ISBN...''>
-				<button id='button' type='button'> Conferma </button>
+				<h1 id='titolo' class = 'center'>Davide's Library</h1>
+				<h1 class = 'center' id = 'testo'>Inserisci libri nel database</h1>
+				<div class = 'center'>
+					<input class='center' id='search' type='text' placeholder='Inserisci un ISBN-10''>
+					<button id='button' type='button'> Aggiungi </button>
+				</div>
 
 				<div id='results'> </div>
-
 				<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
 				<script src='scripts/main.js'></script>
 			";
+
 		}else{
-			echo "EH, VOLEVIH!";
+			echo "accesso negato";
 		}
 	?>
 	
--- login.php
+++ login.php
@@ -1,33 +1,64 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign in</title>
+	<link rel="stylesheet" type="text/css" href="styles/login.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Yeseva One">
+	<style type="text/css">
+		#errore{
+			font-size: 20px;
+			color: red;
+		}
+	</style>
+</head>
+<body>
 
-	require 'connect.php';
+	<h1 id="titolo" class="center">Davide's Library</h1>
+	<div class="login">
+	<div class="lgn_left"><h1>login</h1><p>Inserisci il tuo username e la tua password</p>
+	</div>
+	<div class="lgn_right">
+		<form action="login.php" method="post">
+			<input class="field" type="text" placeholder="Username" id="username" name="username">  
+	  		<input class="field" type="password" placeholder="Password" id="password" name="password">  
+	  		<input class="btn" type="submit" value="LOGIN">
+		</form>
 
-	
+		<?php
+			session_start();
+			session_destroy();
 
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+			if (isset($_POST['username']) AND isset($_POST['password'])) {
 
-	echo "$username";
-	echo "$password";
+				require 'connect.php';
 
-	$query = "SELECT * FROM Dipendenti WHERE nomeUtente='$username' AND password='$password' ";
+				$username = $_POST['username'];
+				$password = $_POST['password'];
 
-	$risultato = mysqli_query($con, $query);
-	$row = mysqli_fetch_array($risultato);
+				$query = "SELECT * FROM Dipendenti WHERE nomeUtente='$username' AND password='$password' ";
 
-	$newURL = "addBooks.php";
+				$risultato = mysqli_query($con, $query);
+				$row = mysqli_fetch_array($risultato);
 
-	if ($row['password'] == "") {
-		echo "Credenziali sbagliate";
-		//login non riuscito
-	}else{
-		session_start();
-		$_SESSION['loginState'] = 'correct';
-		header('Location: '.$newURL);
-	}
+				$newURL = "addBooks.php";
+
+				if ($row['password'] == "") {
+			
+					echo "<p id='errore'>I dati inseriti non sono corretti</p>";
+
+				}else{
+					session_start();
+					$_SESSION['loginState'] = 'correct';
+					header('Location: '.$newURL);
+				}
+				
+			}
+
+		?>
+	  
+	</div>
+	</div>
+</body>
+</html>
 
 
-
-
-
-?>
